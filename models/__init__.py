3fa9d624229852f49638177100e30cf63379578a
Vasu Kataria <vasu@hexerve.com>
2026-03-17 13:23:26 +0530

    feat; upgrade migration

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,10 @@
 from .user import User, RoleEnum
 from .project import Project, ProjectStatus
 from .task import Task, TaskStatus, Priority
+from .team import Team
+from .client import Client
+from .program import Program
+from .portfolio import Portfolio
+from .sprint import Sprint
+from .invoice import Invoice, InvoiceStatus
+from .time_entry import TimeEntry
